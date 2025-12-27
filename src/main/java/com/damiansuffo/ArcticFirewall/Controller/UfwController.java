30f122e6fa6e2b381ba55e41de2a4d7a597be615
Damian <78635584+Aguslxrd@users.noreply.github.com>
2025-12-27 15:15:14 -0300

    ufw controller, thymeleaf template

--- src/main/java/com/damiansuffo/ArcticFirewall/Controller/UfwController.java
+++ src/main/java/com/damiansuffo/ArcticFirewall/Controller/UfwController.java
@@ -1,4 +1,17 @@
 package com.damiansuffo.ArcticFirewall.Controller;
 
+import com.damiansuffo.ArcticFirewall.Ufw.UfwExecutor;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Data
+@Controller
 public class UfwController {
+    private final UfwExecutor ufwExecutor;
+
+    public String index(Model model) {
+        model.addAttribute("status", ufwExecutor.status());
+        return "index";
+    }
 }
