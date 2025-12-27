f59eb8307eff54a0aab24251da567d9aae10d846
Damian <78635584+Aguslxrd@users.noreply.github.com>
2025-12-27 15:19:52 -0300

    ufw se ejecuta correctamente como servicio de linux, renderiza en thymeleaf todas las reglas

--- src/main/java/com/damiansuffo/ArcticFirewall/Controller/UfwController.java
+++ src/main/java/com/damiansuffo/ArcticFirewall/Controller/UfwController.java
@@ -4,12 +4,14 @@ import com.damiansuffo.ArcticFirewall.Ufw.UfwExecutor;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Data
 @Controller
 public class UfwController {
     private final UfwExecutor ufwExecutor;
 
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("status", ufwExecutor.status());
         return "index";
