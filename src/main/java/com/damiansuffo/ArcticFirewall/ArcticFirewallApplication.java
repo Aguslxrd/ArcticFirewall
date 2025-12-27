ea3d70e7c60f975c0407e67ac792f6bc16c591ec
Damian <78635584+Aguslxrd@users.noreply.github.com>
2025-12-27 14:36:16 -0300

    spring base filex

---
+++ src/main/java/com/damiansuffo/ArcticFirewall/ArcticFirewallApplication.java
@@ -0,0 +1,13 @@
+package com.damiansuffo.ArcticFirewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcticFirewallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcticFirewallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damiansuffo/ArcticFirewall/ArcticFirewallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiansuffo.ArcticFirewall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcticFirewallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
